3f9cd0d495e3ea832a9b233e200cd16c6e55abc9
Mirac Gulgonul <mlg556@gmail.com>
2023-01-17 18:47:57 +0300

    added library code and example

--- examples/hx1230_simpletest.py
+++ examples/hx1230_simpletest.py
@@ -2,3 +2,100 @@
 # SPDX-FileCopyrightText: Copyright (c) 2023 Mirac Gulgonul
 #
 # SPDX-License-Identifier: Unlicense
+
+# simple example showing the capabilities of HX1230 with framebuffer
+# adapted from https://github.com/adafruit/Adafruit_CircuitPython_SSD1306/blob/main/examples/ssd1306_framebuftest.py
+
+# import required CircuitPython libraries
+import time
+import board
+import busio
+import digitalio
+
+# import the HX1230 module
+import hx1230
+
+# create the SPI interface, this part depends on your specific board
+# for Raspberry Pico W using SPI0:
+spi = busio.SPI(clock=board.GP18, MOSI=board.GP19)
+# choose a CS (chip select) pin
+cs = digitalio.DigitalInOut(board.GP17)  # Chip select
+# choose a reset pin, this part is optional since HX1230 has sofware reset
+reset = digitalio.DigitalInOut(board.GP20)
+# choose a backlight pin, this is also optional
+backlight = digitalio.DigitalInOut(board.GP16)
+# turn on the backlight
+backlight.switch_to_output(True)
+backlight.value = True
+
+# create the HX1230 class
+display = hx1230.HX1230(spi=spi, cs=cs, reset=reset)
+
+# press ctrl-c to exit
+while True:
+    display.clear()  # clear the display
+
+    # Set a pixel in the origin 0,0 position.
+    display.pixel(0, 0, 1)
+    # Set a pixel in the middle position.
+    display.pixel(display.width // 2, display.height // 2, 1)
+    # Set a pixel in the opposite corner position.
+    display.pixel(display.width - 1, display.height - 1, 1)
+    # don't forget to call show!
+    display.show()
+    time.sleep(2)
+
+    print("Lines test")
+    # we'll draw from corner to corner, lets define all the pair coordinates here
+    corners = (
+        (0, 0),
+        (0, display.height - 1),
+        (display.width - 1, 0),
+        (display.width - 1, display.height - 1),
+    )
+
+    display.fill(0)
+    for corner_from in corners:
+        for corner_to in corners:
+            display.line(corner_from[0], corner_from[1], corner_to[0], corner_to[1], 1)
+    display.show()
+    time.sleep(2)
+
+    print("Rectangle test")
+    display.fill(0)
+    w_delta = display.width / 10
+    h_delta = display.height / 10
+    for i in range(11):
+        display.rect(0, 0, int(w_delta * i), int(h_delta * i), 1)
+    display.show()
+    time.sleep(2)
+
+    print("Text test")
+    display.fill(0)
+    try:
+        display.text("hello world", 0, 0, 1)
+        display.show()
+        time.sleep(1)
+        display.clear()  # clear the display
+        # lets print all the characters from the font5x8.bin font
+        char_width = 5
+        char_height = 8
+        total_lines = display.height // char_height  # 8 lines
+        chars_per_line = display.width // char_width  # 19 chars per line
+        x, y, i = 0, 0, 0
+        for line in range(total_lines):
+            for char in range(chars_per_line):
+                display.text(chr(i), x, y, 1)
+                x += char_width  # increment x
+                i += 1  # increment char index
+            x = 0  # reset x
+            y += char_height  # increment y
+        display.show()
+    except OSError:
+        print(
+            "To test the framebuf font setup, you'll need the font5x8.bin file from "
+            + "https://github.com/adafruit/Adafruit_CircuitPython_framebuf/blob/main/examples/"
+            + " in the same directory as this script"
+        )
+    time.sleep(2)
+    # repeat
--- hx1230.py
+++ hx1230.py
@@ -34,3 +34,196 @@ Implementation Notes
 
 __version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/mlg556/CircuitPython_HX1230.git"
+
+
+from time import sleep
+from micropython import const
+from adafruit_bus_device import spi_device
+
+try:
+    import framebuf
+
+    _FRAMEBUF_FORMAT = framebuf.MONO_VLSB
+except ImportError:
+    import adafruit_framebuf as framebuf
+
+    _FRAMEBUF_FORMAT = framebuf.MVLSB
+
+try:
+    # Used only for typing
+    from typing import Optional
+    import busio
+    import digitalio
+except ImportError:
+    pass
+
+
+# register definitions
+POWER_ON = const(0x2F)  # internal power supply on
+POWER_OFF = const(0x28)  # internal power supply off
+CONTRAST = const(0x80)  # 0x80 + (0~31)
+SEG_NORMAL = const(0xA0)  # SEG remap normal
+SEG_REMAP = const(0xA1)  # SEG remap reverse (flip horizontal)
+DISPLAY_NORMAL = const(0xA4)  # display ram contents
+DISPLAY_TEST = const(0xA5)  # all pixels on
+INVERT_OFF = const(0xA6)  # not inverted
+INVERT_ON = const(0xA7)  # inverted
+DISPLAY_ON = const(0xAF)  # display on
+DISPLAY_OFF = const(0xAE)  # display off
+SCAN_START_LINE = const(0x40)  # 0x40 + (0~63)
+COM_NORMAL = const(0xC0)  # COM remap normal
+COM_REMAP = const(0xC8)  # COM remap reverse (flip vertical)
+SW_RESET = const(0xE2)  # connect RST pin to GND to rely on software reset
+# NOP = const(0xE3) # no operation
+DATA = const(0x80)  # the msb in the 9-bit spi mode, command = 0, data = 1
+THREE_BITS = const(0x07)  # 0~7
+FOUR_BITS = const(0x0F)  # 0~15
+FIVE_BITS = const(0x1F)  # 0~31
+SIX_BITS = const(0x3F)  # 0~63
+
+# DDRAM addresses
+COL_ADDR = const(0x10)  # x pos (0~95)
+PAGE_ADDR = const(0xB0)  # y pos, 9 pages of 8 rows (0~8)
+
+# Display dimensions
+WIDTH = const(0x60)  # 96 cols
+HEIGHT = const(0x44)  # 68 rows
+# 9 pages (68 / 8 = 8.5, rounded up to 9) the last page is only half visible
+PAGES = const(9)
+
+
+class HX1230(framebuf.FrameBuffer):
+    """
+    Main class for HX1230 display driver.
+
+    :param spi: the SPI peripheral to use
+    :param cs: the chip-select pin to use
+    :param reset: the reset pin to use
+    :param baudrate: baudrate, maximum 4_000_000
+    """
+
+    def __init__(
+        self,
+        spi: busio.SPI,
+        cs: digitalio.DigitalInOut,
+        reset: Optional[digitalio.DigitalInOut],
+        baudrate=2_000_000,
+    ):
+        self.spi_device = spi_device.SPIDevice(
+            spi=spi, chip_select=cs, baudrate=baudrate, polarity=0, phase=0
+        )
+        self.width = WIDTH
+        self.height = HEIGHT
+        self.reset_pin = reset
+        self.cs_pin = cs
+        self.framebuffer = bytearray(PAGES * WIDTH)  # initialize framebuffer
+        if self.reset_pin:
+            self.reset_pin.switch_to_output(value=True)
+        super().__init__(self.framebuffer, WIDTH, HEIGHT, _FRAMEBUF_FORMAT)
+        self._power = False
+        # Let's get moving!
+        self.init_display()
+
+    def init_display(self) -> None:
+        """Initialize the display"""
+        self.reset()
+        self.power_on()
+        self.contrast(31)  # maximum contrast
+        self.invert(False)
+        self.test(False)
+        self.orientation(False, False)  # no rotation
+        self.display(True)  # switch on
+        self.start_line(0)  # set line to 0
+        self.clear()  # clear
+        self.show()
+
+    def reset(self) -> None:
+        """Reset the display"""
+        if self.reset_pin:
+            self.reset_pin.value = False
+            sleep(0.05)  # 50ms, recommended is between 10 and 100 ms
+            self.reset_pin.value = True
+            sleep(0.05)
+
+    def power_on(self) -> None:
+        """Turn the display ON"""
+        self.write_command(POWER_ON)
+
+    def power_off(self) -> None:
+        """Turn the display OFF"""
+        self.write_command(POWER_OFF)
+
+    def contrast(self, contrast: int) -> None:
+        """Adjust the contrast, a value between 0~31"""
+        self.write_command(CONTRAST | (contrast & FIVE_BITS))
+
+    def invert(self, invert=True) -> None:
+        """Invert all pixels on the display"""
+        self.write_command(INVERT_ON if invert else INVERT_OFF)
+
+    def display(self, on=True) -> None:
+        """Set the display on"""
+        self.write_command(DISPLAY_ON if on else DISPLAY_OFF)
+
+    def test(self, on=True) -> None:
+        """Display test pattern"""
+        self.write_command(DISPLAY_TEST if on else DISPLAY_NORMAL)
+
+    def orientation(self, seg_remap=False, com_remap=False) -> None:
+        """Set the orientation, to rotate 180, set both to True."""
+        self.write_command(SEG_REMAP if seg_remap else SEG_NORMAL)
+        self.write_command(COM_REMAP if com_remap else COM_NORMAL)
+
+    def position(self, x: int, y: int) -> None:
+        """Position the cursor, x between 0~95 and y between 0~9"""
+        self.write_command(PAGE_ADDR | y)  # set y pos (0~8)
+        self.write_command(COL_ADDR | ((x >> 4) & THREE_BITS))  # set x pos high 3 bits
+        self.write_command(x & FOUR_BITS)  # set x pos low 4 bits
+
+    def start_line(self, line=0) -> None:
+        """Set the start line, between 0~63"""
+        self.write_command(SCAN_START_LINE + (line & SIX_BITS))
+
+    def clear(self) -> None:
+        """Clear the display"""
+        self.fill(0)
+        self.show()
+
+    def show(self) -> None:
+        """Update the display"""
+        self.position(0, 0)  # why 0,0?
+        self.write_framebuf()
+
+    def write_command(self, command: int) -> None:
+        """Sends a single command"""
+        # allocate 9 bits to be sent
+        # 9-bit SPI, MSB = D/C, 8 data bits, 7 padding bits
+        command_array = bytearray(2)
+        # send 2 bytes left aligned with padding bits
+        command_array[0] = 0  # MSB is D/C, in this case C(control)
+        command_array[0] |= command >> 1  # high 7 bits of value
+        command_array[1] = (command & 1) << 7  # low 1 bit of value
+        self.cs_pin.value = False
+        with self.spi_device as spi:
+            spi.write(command_array)
+        self.cs_pin.value = True
+
+    def write_framebuf(self) -> None:
+        """Sends the framebuffer via SPI"""
+        # for each 8 bytes of the framebuffer,
+        # send 9 bytes (include a DC bit for each chunk)
+        for i in range(0, PAGES * WIDTH, 8):
+            chunk = self.framebuffer[i : i + 8]
+            command = bytearray(9)
+            for j in range(8):
+                command[j] |= 1 << (7 - j)  # DC bit
+                if j == 7:
+                    # DC on prev 7th byte means this one is no change
+                    command[j + 1] = chunk[j]
+                else:
+                    command[j] |= chunk[j] >> (j + 1)
+                    command[j + 1] |= (chunk[j] << (7 - j)) & 0xFF
+            self.cs_pin.value = False
+            with self.spi_device as spi:
+                spi.write(command)
+            self.cs_pin.value = True
