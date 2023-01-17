689669ec7ca07909ad09260c90b818f86136f366
Mirac Gulgonul <mlg556@gmail.com>
2023-01-17 18:43:33 +0300

    initial commit from cookiecutter

---
+++ docs/examples.rst
@@ -0,0 +1,8 @@
+Simple test
+------------
+
+Ensure your device works with this simple test.
+
+.. literalinclude:: ../examples/hx1230_simpletest.py
+    :caption: examples/hx1230_simpletest.py
+    :linenos:
---
+++ hx1230.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2023 Mirac Gulgonul
+#
+# SPDX-License-Identifier: MIT
+"""
+`hx1230`
+================================================================================
+
+CircuitPython framebuf driver for HX1230 96x68 monochrome LCD displays using SPI.
+
+
+* Author(s): Mirac Gulgonul
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+.. todo:: Add links to any specific hardware product page(s), or category page(s).
+  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://circuitpython.org/downloads
+
+.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
+  based on the library's use of either.
+
+# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+"""
+
+# imports
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/mlg556/CircuitPython_HX1230.git"
